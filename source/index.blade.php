41d3a2f47c31464c7f43dac8e16212d3101ebc32
kekebeen <bbabic081@gmail.com>
2020-12-12 19:14:11 +0100

    feat(homepage): add default welcome page

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,15 @@
 @extends('_layouts.master')
 
 @section('body')
-<div class="p-8">
-    <h1 class="text-3xl font-bold">Hello world!</h1>
+<div class="container p-16 mx-auto">
+    <h1 class="mb-4 text-3xl font-semibold leading-relaxed">Hello world!</h1>
+    <div class="aspect-w-4 aspect-h-1">
+        <iframe 
+            src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen>
+        </iframe>
+    </div>
 </div>
 @endsection
