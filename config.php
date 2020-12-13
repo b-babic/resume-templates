f2f3548d501580304750ce5f35ac44517b60ed19
kekebeen <bbabic081@gmail.com>
2020-12-13 22:03:40 +0100

    feat(config): add default random generated user info

--- config.php
+++ config.php
@@ -1,9 +1,19 @@
 <?php
 
+$faker = Faker\Factory::create();
+
 return [
     'production' => false,
     'baseUrl' => '',
     'title' => 'Jigsaw',
     'description' => 'Website description.',
+    'author' => $faker->name,
+    'jobTitle' => $faker->jobTitle,
+    'email' => $faker->email,
+    'telephone' => $faker->phoneNumber,
+    'githubUrl' => $faker->url,
+    'githubHandle' => $faker->word,
+    'linkedinUrl' => $faker->url,
+    'linkedinHandle' => $faker->word,
     'collections' => [],
 ];
