420a23be64847f484d0f054bac5d4bc21b2033b2
kekebeen <bbabic081@gmail.com>
2020-12-14 21:53:44 +0100

    style(about): fix section separator width issue

--- source/_partials/first/about.blade.php
+++ source/_partials/first/about.blade.php
@@ -1,4 +1,4 @@
-<section>
+<section class="mb-16">
 	<h3 class="flex items-center space-x-6 mb-6">
 		<div class="flex items-center space-x-4">
 			@include('_icons.first-template.profile')
@@ -8,7 +8,7 @@
 			Profile
 			</span>	
 		</div>
-		<div class="w-full h-px bg-gradient-to-r from-teal-400 to-indigo-500 rounded-lg"></div>
+		<div class="flex-1 h-px bg-gradient-to-r from-teal-400 to-indigo-500 rounded-lg"></div>
 	</h3>
 	{!! $about->first() !!}
 </section>
