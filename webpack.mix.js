599433661b19f1f7edb12049aebf49964128d7e4
kekebeen <bbabic081@gmail.com>
2020-12-25 20:18:03 +0100

    refactor(webpack): remove jigsaw plugin

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,9 @@
 const mix = require('laravel-mix');
-require('laravel-mix-jigsaw');
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
 
-mix.jigsaw()
+mix
     .js('source/_assets/js/main.js', 'js')
     .postCss('source/_assets/css/main.css', 'css', [
         require('postcss-import'),
