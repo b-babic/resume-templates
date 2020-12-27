727015566a43d5c24e2696223a0f836913123662
kekebeen <bbabic081@gmail.com>
2020-12-27 21:01:00 +0100

    chore(template): pull latest template

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,14 +5,10 @@ mix.setPublicPath('source/assets/build');
 
 mix
     .js('source/_assets/js/main.js', 'js')
-    .postCss('source/_assets/css/main.css', 'css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-    ])
     .options({
         processCssUrls: false,
     })
-  .version();
+    .version();
 
 if(mix.inProduction()) {
   mix.sourceMaps();
